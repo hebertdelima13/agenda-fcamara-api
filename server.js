4f8393ecfc1a77233fbd44af09350c18d9bf857d
Hebert Fernandes <hebertdelima13@gmail.com>
2021-09-03 16:45:50 -0300

    modificando o server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const { compareSync } = require('bcrypt');
 
+const apiRoutes = require('./src/routes');
+
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -23,9 +25,7 @@ server.use(express.urlencoded({extend: true}));
 
 server.use(express.static(__dirname+'/public'));
 
-server.get('/ping', (req, res) => {
-    res.json({pong: true});
-});
+server.use('/', apiRoutes);
 
 server.listen(process.env.PORT, () => {
     console.log(`- Rodando no endereço: ${process.env.BASE}`);
