7f4537584d43258478e832f4bff3cf5238c1bd29
Hebert Fernandes <hebertdelima13@gmail.com>
2021-09-04 11:06:49 -0300

    login e cadastro pronto

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,16 @@
+const User = require('../models/User');
+
 module.exports = {
     info: async (req, res) => {
 
+        let token = req.query.token;
+
+        const user = await User.findOne({token}); 
+
+        res.json({
+            name: user.name,
+            email: user.email,
+        });
     },
 
     editAction: async (req, res) => {
